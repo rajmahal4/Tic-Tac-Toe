2f51ce6ddace81f27260e2c24d0382f7cc805755
rajmahal4 <rsingh6@mail.sfsu.edu>
2020-04-13 13:47:23 -0700

    Working version needs styles

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const gameArray = [];
 const gameBoard = (() =>{
     const board = document.querySelector("#board");
     board.addEventListener("click", (e)=>{
-
+        const one = document.querySelector(".player1");
+        const two = document.querySelector(".player2");
         const {target} = e;
         const id = target.id
 
@@ -13,6 +14,8 @@ const gameBoard = (() =>{
         let text = document.createElement("h1");
         target.classList.add("mark")      
         target.innerHTML = text.textContent = player1.mark
+        two.style.cssText = "";
+        one.style.cssText = "border: solid 5px"
         flag = false;
         gameArray[`${target.id}`] = {player1};
         console.log(target)
@@ -21,6 +24,8 @@ const gameBoard = (() =>{
 
     else if(target.id !== "board" && flag === false && target.innerHTML === ""){
         let text = document.createElement("h1");
+        one.style.cssText = ""
+        two.style.cssText = "border: solid 5px"
         target.classList.add("mark")      
         target.innerHTML = text.textContent = player2.mark
 
@@ -53,6 +58,11 @@ function Reset(){
 
         gameArray.pop();
     }
+    const one = document.querySelector(".player1");
+    const two = document.querySelector(".player2");
+    one.style.cssText = "";
+    two.style.cssText = "";
+
     flag = true;
     
 }
@@ -62,26 +72,55 @@ function checkBoard(){
     const board = document.querySelector("#board");
 
     if(board.children[0].innerHTML === "X" && board.children[4].innerHTML === "X" && board.children[8].innerHTML === "X"){
-        alert("You win");
+        alert("Player One Wins");
         Reset();
         return
     }else if(board.children[0].innerHTML === "X" && board.children[1].innerHTML === "X" && board.children[2].innerHTML === "X"){
-        alert("You Win")
+        alert("Player One Wins")
         Reset();
         return
     }else if(board.children[0].innerHTML === "X" && board.children[3].innerHTML === "X" && board.children[6].innerHTML === "X"){
-        alert("You Win")
+        alert("Player One Wins")
         Reset();
         return
     }else if(board.children[6].innerHTML === "X" && board.children[7].innerHTML === "X" && board.children[8].innerHTML === "X"){
-        alert("You Win")
+        alert("Player One Wins")
         Reset();
         return
     }else if(board.children[2].innerHTML === "X" && board.children[5].innerHTML === "X" && board.children[8].innerHTML === "X" || board.children[2].innerHTML === "X" && board.children[6].innerHTML === "X" && board.children[4].innerHTML === "X"){
-        alert("You Win")
+        alert("Player One Wins")
+        Reset();
+        return
+    }else if(board.children[3].innerHTML === "X" && board.children[4].innerHTML === "X" && board.children[5].innerHTML === "X" || board.children[1].innerHTML === "X" && board.children[4].innerHTML === "X" && board.children[7].innerHTML === "X"){
+        alert("Player One Wins")
+        Reset();
+        return
+    }if(board.children[0].innerHTML === "0" && board.children[4].innerHTML === "0" && board.children[8].innerHTML === "0"){
+        alert("Player Two Wins");
+        Reset();
+        return
+    }else if(board.children[0].innerHTML === "0" && board.children[1].innerHTML === "0" && board.children[2].innerHTML === "0"){
+        alert("Player Two Wins")
+        Reset();
+        return
+    }else if(board.children[0].innerHTML === "0" && board.children[3].innerHTML === "0" && board.children[6].innerHTML === "0"){
+        alert("Player Two Wins")
+        Reset();
+        return
+    }else if(board.children[6].innerHTML === "0" && board.children[7].innerHTML === "0" && board.children[8].innerHTML === "0"){
+        alert("Player Two Wins")
+        Reset();
+        return
+    }else if(board.children[2].innerHTML === "0" && board.children[5].innerHTML === "0" && board.children[8].innerHTML === "0" || board.children[2].innerHTML === "0" && board.children[6].innerHTML === "0" && board.children[4].innerHTML === "0"){
+        alert("Player Two Wins")
+        Reset();
+        return
+    }else if(board.children[3].innerHTML === "0" && board.children[4].innerHTML === "0" && board.children[5].innerHTML === "0" || board.children[1].innerHTML === "0" && board.children[4].innerHTML === "0" && board.children[7].innerHTML === "0"){
+        alert("Player Two Wins")
         Reset();
         return
     }
+    
     
 }
 console.log(gameArray.length)
